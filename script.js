73e5b4f17db782dbab3ee37f2e9af880624a79a8
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-06-27 01:43:05 +0200

    Add navigation animations

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// control navigation animation
+function changeNavAnimation(direction1, direction2) {
+  navItems.forEach((nav, index) => {
+    nav.classList.replace(`slide-${direction1}-${index + 1}`, `slide-${direction2}-${index + 1}`);
+  })
+}
 
 function toggleNav() {
   // toggle manu bars
@@ -13,19 +21,24 @@ function toggleNav() {
   overlay.classList.toggle('overlay-active');
   if (overlay.classList.contains('overlay-active')) {
     // overlay animate in
-    overlay.classList.remove('overlay-slide-left');
-    overlay.classList.add('overlay-slide-right');
+    overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+    // nav items animate in
+    changeNavAnimation('out', 'in');
   } else {
     // overlay animate out
-    overlay.classList.remove('overlay-slide-right');
-    overlay.classList.add('overlay-slide-left');
+    overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+    // nav items animate out
+    changeNavAnimation('in', 'out');
   }
 }
 
 // event listeners
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
-nav3.addEventListener('click', toggleNav);
-nav4.addEventListener('click', toggleNav);
-nav5.addEventListener('click', toggleNav);
+navItems.forEach(navItem => navItem.addEventListener('click', toggleNav))
+
+// make nav visible again after 1s from page load
+window.addEventListener('DOMContentLoaded', ()=> {
+  setTimeout(() => {
+    overlay.hidden = false
+  }, 1000);
+})
